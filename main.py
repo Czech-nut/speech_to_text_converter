fcbee20fcad88d1005b27c3549a3ca9439a5b45e
Czech-nut <valeria.shcherrba@gmail.com>
2021-06-25 10:19:39 +0300

    main refactoring

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 import os
+from dotenv import load_dotenv
+
 from google.cloud import speech_v1
-from google.cloud.speech_v1 import enums
 from google.cloud import storage
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "credentials.json"
-bucket_name = "podcast_to_text"
-gcloud_file_name = "podcast.mp3"
+
+load_dotenv()
+
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+GCLOUD_FILE_NAME = os.getenv('GCLOUD_FILE_NAME')
 
 
-def upload_audio_to_gcloud(bucket_name, file_name, gcloud_file_name):
+def upload_audio_to_gcloud(file_name):
     """
     Upload audio file to Google Cloud Storage.
-    :param bucket_name: str, name of the bucket in Google Cloud Storage, where the file will be stored
-    :param file_name: str, name of the file to upload
-    :return: URI of that audio file, stored in Google Cloud Storage
+    :param file_name: Name of the file to upload.
+    :return: URI of that audio file, stored in Google Cloud Storage.
     """
     storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(gcloud_file_name)
+    bucket = storage_client.get_bucket(BUCKET_NAME)
+    blob = bucket.blob(GCLOUD_FILE_NAME)
     blob.upload_from_filename("audio/{}".format(file_name))
     print('File {} uploaded to GCloud.'.format(file_name))
 
-    storage_uri = "gs://{}/{}".format(bucket_name, gcloud_file_name)
-    sample_long_running_recognize(storage_uri)
+    storage_uri = "gs://{}/{}".format(BUCKET_NAME, GCLOUD_FILE_NAME)
+    transcribe_long_audio_file(storage_uri, file_name)
 
 
-def sample_long_running_recognize(storage_uri):
+def transcribe_long_audio_file(storage_uri, file_name):
     """
-    Transcribe long audio file from Cloud Storage using asynchronous speech
-    recognition
-
-    Args:
-      storage_uri URI for audio file in Cloud Storage, e.g. gs://[BUCKET]/[FILE]
+    Transcribe long audio file from Cloud Storage using asynchronous speech recognition.
+    :param storage_uri: URI for audio file in Cloud Storage, e.g. gs://[BUCKET]/[FILE].
+    :param file_name: Name of the file to upload.
     """
-
     client = speech_v1.SpeechClient()
 
     # Sample rate in Hertz of the audio data sent
@@ -43,8 +43,7 @@ def sample_long_running_recognize(storage_uri):
     language_code = "en-US"
 
     # Encoding of audio data sent. This sample sets this explicitly.
-    # This field is optional for FLAC and WAV audio formats.
-    encoding = enums.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED
+    encoding = speech_v1.enums.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED
     config = {
         "sample_rate_hertz": sample_rate_hertz,
         "language_code": language_code,
@@ -64,12 +63,15 @@ def sample_long_running_recognize(storage_uri):
             alternative = result.alternatives[0]
             text_file.write("{} ".format(alternative.transcript))
 
-    print("Done!\n")
 
-
-if __name__ == '__main__':
+def run():
     all_files = os.listdir("audio")
     print("Total number of audio files: {}".format(len(all_files)))
     for file_name in all_files:
-        print("{}. {} - uploading to GCloud".format(all_files.index(file_name)+1, file_name))
-        upload_audio_to_gcloud(bucket_name, file_name, gcloud_file_name)
+        print("{}. {} - uploading to GCloud".format(all_files.index(file_name), file_name))
+        upload_audio_to_gcloud(file_name)
+    print("Processing finished!")
+
+
+if __name__ == '__main__':
+    run()
